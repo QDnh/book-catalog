8879adc1890492514d91ceaaea175cb2a52444ba
QDnh <qdonahe1@buffs.wtamu.edu>
2026-09-13 21:17:23 -0500

    Add Publisher, Book, and Review models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    datepublished = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    body = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.body
